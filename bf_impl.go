df648213aa9ccd090df71d390d79b74247f91a28
VBPROGER <vsevolodb1104@yandex.ru>
2024-08-17 23:19:58 +0300

    Add more return params to `parse()`

--- bf_impl.go
+++ bf_impl.go
@@ -7,17 +7,18 @@ import (
 )
 
 const MEMORY_SIZE = 30000
+const NULL_IDX = -1
 
 func unsupported(pos int, op string) error {
   return fmt.Errorf("operation unsupported at pos %d: %s", pos, op)
 }
 
-func parse(b []byte) ([MEMORY_SIZE]int, error){
+func parse(b []byte) ([MEMORY_SIZE]int, int, error, int){
   var mem [MEMORY_SIZE]int
   i := 0
   for idx, symbol := range b {
     if i >= len(mem) || i < 0 {
-      return mem, fmt.Errorf("out of memory at pos %d", idx)
+      return mem, i, fmt.Errorf("out of memory at pos %d", idx), idx
     }
     switch symbol {
       case '+': mem[i] += 1
@@ -25,21 +26,21 @@ func parse(b []byte) ([MEMORY_SIZE]int, error){
       case '>': i += 1
       case '<': i -= 1
       case '.': fmt.Printf("%s", string(rune(mem[i])))
-      case ',': return mem, unsupported(idx, ",")
-      case '[': return mem, unsupported(idx, "[")
-      case ']': return mem, unsupported(idx, "]")
+      case ',': return mem, i, unsupported(idx, ","), idx
+      case '[': return mem, i, unsupported(idx, "["), idx
+      case ']': return mem, i, unsupported(idx, "]"), idx
       case '\n': // Newline encountered. Just ignore it.
       default:
-        return mem, fmt.Errorf("unknown symbol encountered at pos %d: %s", idx, string(rune(symbol)))
+        return mem, i, fmt.Errorf("unknown symbol encountered at pos %d: %s", idx, string(rune(symbol))), idx
     }
   }
-  return mem, nil
+  return mem, i, nil, NULL_IDX
 }
 
 func main(){
   reader := bufio.NewReader(os.Stdin)
   t, _ := reader.ReadBytes('\n')
-  _, err := parse(t)
+  _, _, err, _ := parse(t)
   if err != nil {
     fmt.Printf("An error has occured while executing the script:\n%s\n", err.Error())
   }
--- bf_impl_test.go
+++ bf_impl_test.go
@@ -6,31 +6,31 @@ import (
 )
 
 func TestParseUnknownSymbol(t *testing.T){
-  _, err := parse([]byte("?"))
+  _, _, err, _ := parse([]byte("?"))
   if err == nil {
     t.Errorf("\"Unknown symbol\" error missing")
   }
 }
 func TestParseUnsupportedSymbol(t *testing.T){
-  _, err := parse([]byte("[]"))
+  _, _, err, _ := parse([]byte("[]"))
   if err == nil {
     t.Errorf("\"Unsupported symbol\" error missing")
   }
 }
 func TestParseNull(t *testing.T){
-  _, err := parse([]byte(""))
+  _, _, err, _ := parse([]byte(""))
   if err != nil {
     t.Errorf("Got an error when passing no data")
   }
 }
 func TestParseOutOfMemory(t *testing.T){
-  _, err := parse([]byte(strings.Repeat(">", MEMORY_SIZE + 1)))
+  _, _, err, _ := parse([]byte(strings.Repeat(">", MEMORY_SIZE + 1)))
   if err == nil {
     t.Errorf("No error when running out of memory")
   }
 }
 func TestParseNegativePointer(t *testing.T){
-  _, err := parse([]byte(strings.Repeat("<", MEMORY_SIZE + 1)))
+  _, _, err, _ := parse([]byte(strings.Repeat("<", MEMORY_SIZE + 1)))
   if err == nil {
     t.Errorf("Negative pointer allowed")
   }
