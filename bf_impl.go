5dd58fbd5cd5b256458cec1f82258c6e181bad10
VBPROGER <vsevolodb1104@yandex.ru>
2024-08-17 23:25:08 +0300

    Improve error messages

--- bf_impl.go
+++ bf_impl.go
@@ -4,13 +4,14 @@ import (
   "fmt"
   "os"
   "bufio"
+  "log"
 )
 
 const MEMORY_SIZE = 30000
 const NULL_IDX = -1
 
-func unsupported(pos int, op string) error {
-  return fmt.Errorf("operation unsupported at pos %d: %s", pos, op)
+func unsupported(op string) error {
+  return fmt.Errorf("operation unsupported: %s", op)
 }
 
 func parse(b []byte) ([MEMORY_SIZE]int, int, error, int){
@@ -18,7 +19,7 @@ func parse(b []byte) ([MEMORY_SIZE]int, int, error, int){
   i := 0
   for idx, symbol := range b {
     if i >= len(mem) || i < 0 {
-      return mem, i, fmt.Errorf("out of memory at pos %d", idx), idx
+      return mem, i, fmt.Errorf("out of memory"), idx
     }
     switch symbol {
       case '+': mem[i] += 1
@@ -26,12 +27,12 @@ func parse(b []byte) ([MEMORY_SIZE]int, int, error, int){
       case '>': i += 1
       case '<': i -= 1
       case '.': fmt.Printf("%s", string(rune(mem[i])))
-      case ',': return mem, i, unsupported(idx, ","), idx
-      case '[': return mem, i, unsupported(idx, "["), idx
-      case ']': return mem, i, unsupported(idx, "]"), idx
+      case ',': return mem, i, unsupported(","), idx
+      case '[': return mem, i, unsupported("["), idx
+      case ']': return mem, i, unsupported("]"), idx
       case '\n': // Newline encountered. Just ignore it.
       default:
-        return mem, i, fmt.Errorf("unknown symbol encountered at pos %d: %s", idx, string(rune(symbol))), idx
+        return mem, i, fmt.Errorf("unknown symbol encountered: %s", string(rune(symbol))), idx
     }
   }
   return mem, i, nil, NULL_IDX
@@ -40,8 +41,8 @@ func parse(b []byte) ([MEMORY_SIZE]int, int, error, int){
 func main(){
   reader := bufio.NewReader(os.Stdin)
   t, _ := reader.ReadBytes('\n')
-  _, _, err, _ := parse(t)
+  _, i, err, pos := parse(t)
   if err != nil {
-    fmt.Printf("An error has occured while executing the script:\n%s\n", err.Error())
+    log.Fatalf("Error at position %d in slot %d:\n%s\n", pos, i, err.Error())
   }
 }
